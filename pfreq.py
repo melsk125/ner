18e789d8f034fbd9df1caf8ed1ba91c5f430288c
Panot Chaimongkol <mel.sk125@gmail.com>
2011-11-25 06:34:38 +0900

    Modified pfreq.py so that it include count of whole word appearance

--- pfreq.py
+++ pfreq.py
@@ -16,18 +16,20 @@ words = lib.collapse([entry.split() for entry in list])
 termset = set(terms)
 wordset = set(words)
 
-worddict = dict([(el, (0,0,0,0)) for el in wordset])
+worddict = dict([(el, (0,0,0,0,0)) for el in wordset])
 
 for term in termset:
 	term = term.split()
 	for i in range(len(term)):
-		f,a,b,c = worddict[term[i]]
+		f,a,b,c,d = worddict[term[i]]
+		if len(term) == 1:
+			worddict[term[i]] = f+1,a+1,b,c,d
 		if i == 0:
-			worddict[term[i]] = f+1,a+1,b,c
+			worddict[term[i]] = f+1,a,b+1,c,d
 		elif i == (len(term)-1):
-			worddict[term[i]] = f+1,a,b,c+1
+			worddict[term[i]] = f+1,a,b,c,d+1
 		else:
-			worddict[term[i]] = f+1,a,b+1,c
+			worddict[term[i]] = f+1,a,b,c+1,d
 
-for word, (f,a,b,c) in sorted(worddict.items(), key=lambda entry: entry[1][0], reverse=True):
-	print word + '\t' + str(f) + '\t' + str(a) + '\t' + str(b) + '\t' + str(c)
+for word, (f,a,b,c,d) in sorted(worddict.items(), key=lambda entry: entry[1][0], reverse=True):
+	print word + '\t' + str(f) + '\t' + str(a) + '\t' + str(b) + '\t' + str(c) + '\t' + str(d)
