839262dee2db3555f92d1aa694c10bdacae4938b
Panot Chaimongkol <mel.sk125@gmail.com>
2011-12-15 17:19:51 +0900

    Modified features.py
    Change some incorrect spelling in names.
    Added functions to return the whole list of features.

--- features.py
+++ features.py
@@ -1,4 +1,6 @@
 import re
+from porter import PorterStemmer
+p = PorterStemmer()
 
 def lcase(text):
     return text.lower()
@@ -6,11 +8,12 @@ def lcase(text):
 def prefixes(text):
     return [text[:3], text[:4], text[:5]]
 
-def suffixed(text):
+def suffixes(text):
     return [text[-3:], text[-4:], text[-5:]]
 
 def stem(text):
-# To be implemented
+    if text.isalpha():
+        return p.stem(text.lower(), 0, len(text)-1)
     return text
 
 def is_pair_of_digits(text):
@@ -38,12 +41,12 @@ def digits_and_slashes(text):
         return True
     return False
 
-def digites_and_colons(text):
+def digits_and_colons(text):
     if re.match("^[0-9,]+$", text):
         return True
     return False
 
-def digites_and_dots(text):
+def digits_and_dots(text):
     if re.match("^[0-9.]+$", text):
         return True
     return False
@@ -115,3 +118,17 @@ def collapsed_pattern(text):
     text = re.sub("[0-9]+", "0", text)
     return text
 
+def features(text):
+    return [text, lcase(text), prefixes(text), suffixes(text), stem(text), is_pair_of_digits(text), is_four_digits(text), letters_and_digits(text), digits_and_hyphens(text), digits_and_slashes(text), digits_and_colons(text), digits_and_dots(text), ucase_and_dots(text), initial_ucase(text), only_ucase(text), only_lcase(text), only_digits(text), only_non_alnum(text), contains_ucase(text), contains_lcase(text), contains_digits(text), contains_non_alnum(text), date_regex(text), pattern(text), collapsed_pattern(text)]
+
+def print_features(text):
+    feats = features(text)
+    i = 2
+    ret = "f[01]=" + text
+    for f in feats[1:]:
+        if type(f) == str:
+            ret += ("\tf[%02d]=%s" % (i, f))
+        elif type(f) == bool and f == True:
+            ret += ("\tf[%02d]" % i)
+        i += 1
+    return ret
